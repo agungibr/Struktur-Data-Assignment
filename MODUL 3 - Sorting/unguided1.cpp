5a372ae73a07e84ed7bdd4e6a7adfa8cdc5e62d9
agungibr <agungibr@outlook.co.id>
2024-04-11 19:53:32 +0700

    unguided 1 fix

--- MODUL 3 - Sorting/unguided1.cpp
+++ MODUL 3 - Sorting/unguided1.cpp
@@ -1,22 +1,24 @@
-#include <iostream>
-using namespace std;
+#include <iostream> //input preprocessor directive
+using namespace std; //supaya tidak perlu menggunakan std:: disetiap input object or function
 
-void selection (float arr[], float n) {
+void selection (float arr[], float n) { //function untuk selection sort
 	int i, j, small;
 
 	for (i = 0; i < n-1; i++) {
-		small = i;
+		small = i; //minimum elemen di array yang belum di sorting
 
 		for (j = i+1; j < n; j++)
-		if (arr[j] > arr[small])
+		if (arr[j] > arr[small]) //karena descending maka menggunakan >
 			small = j;
 
+		//swap minimum elemen dengan elemen pertama
 		float temp = arr[small];
 		arr[small] = arr[i];
 		arr[i] = temp;
 	}
 }
 
+//function untuk mencetak array
 void printArr (float a[], float n) {
 	int i;
 
@@ -24,16 +26,19 @@ void printArr (float a[], float n) {
 		cout << a[i] << " ";
 }
 
-int main() {
+int main() { //main program
 	float a[] = {3.8, 2.9, 3.3, 4.0, 2.4};
 	float n = sizeof(a) / sizeof(a[0]);
 
+	//print before
 	cout << "Indeks Prestasi Mahasiswa sebelum di sorting: " << endl;
 	printArr (a, n);
 	cout << endl << endl;
 
+	//proses sorting dengan variable yang sudah dibuat
 	selection (a, n);
-	
+
+	//print after
 	cout << "Indeks Prestasi Mahasiswa setelah di sorting: " << endl;
 	printArr(a, n);
 
