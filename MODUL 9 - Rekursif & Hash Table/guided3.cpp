7351c2839ad70876c1757ae0450d9722aadbd4eb
agungibr <agungibr@outlook.co.id>
2024-05-30 20:46:01 +0700

    fix guided 3

--- MODUL 9 - Rekursif & Hash Table/guided3.cpp
+++ MODUL 9 - Rekursif & Hash Table/guided3.cpp
@@ -1,4 +1,3 @@
-// Guided 3
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,6 +9,7 @@ class HashNode {
 public:
     string name;
     string phone_number;
+
     HashNode(string name, string phone_number) {
         this->name = name;
         this->phone_number = phone_number;
@@ -19,20 +19,21 @@ public:
 class HashMap {
 private:
     vector<HashNode*> table[TABLE_SIZE];
+
 public:
     int hashFunc(string key) {
         int hash_val = 0;
-        for (size_t i = 0; i < key.length(); ++i) {
-            hash_val += key[i];
+        for (char c : key) {
+            hash_val += c;
         }
         return hash_val % TABLE_SIZE;
     }
 
     void insert(string name, string phone_number) {
         int hash_val = hashFunc(name);
-        for (size_t i = 0; i < table[hash_val].size(); ++i) {
-            if (table[hash_val][i]->name == name) {
-                table[hash_val][i]->phone_number = phone_number;
+        for (auto node : table[hash_val]) {
+            if (node->name == name) {
+                node->phone_number = phone_number;
                 return;
             }
         }
@@ -41,32 +42,30 @@ public:
 
     void remove(string name) {
         int hash_val = hashFunc(name);
-        for (size_t i = 0; i < table[hash_val].size(); ++i) {
-            if (table[hash_val][i]->name == name) {
-                delete table[hash_val][i]; // Free memory allocated for the node
-                table[hash_val].erase(table[hash_val].begin() + i);
+        for (auto it = table[hash_val].begin(); it != table[hash_val].end(); it++) {
+            if ((*it)->name == name) {
+                table[hash_val].erase(it);
                 return;
             }
         }
-        cout << "Node with name '" << name << "' not found." << endl;
     }
 
     string searchByName(string name) {
         int hash_val = hashFunc(name);
-        for (size_t i = 0; i < table[hash_val].size(); ++i) {
-            if (table[hash_val][i]->name == name) {
-                return table[hash_val][i]->phone_number;
+        for (auto node : table[hash_val]) {
+            if (node->name == name) {
+                return node->phone_number;
             }
         }
         return "";
     }
-    
+
     void print() {
-        for (int i = 0; i < TABLE_SIZE; ++i) {
+        for (int i = 0; i < TABLE_SIZE; i++) {
             cout << i << ": ";
-            for (size_t j = 0; j < table[i].size(); ++j) {
-                if (table[i][j] != NULL) {
-                    cout << "[" << table[i][j]->name << ", " << table[i][j]->phone_number << "]";
+            for (auto pair : table[i]) {
+                if (pair != nullptr) {
+                    cout << "[" << pair->name << ", " << pair->phone_number << "]";
                 }
             }
             cout << endl;
@@ -79,12 +78,16 @@ int main() {
     employee_map.insert("Mistah", "1234");
     employee_map.insert("Pastah", "5678");
     employee_map.insert("Ghana", "91011");
+
     cout << "Nomer Hp Mistah : " << employee_map.searchByName("Mistah") << endl;
     cout << "Phone Hp Pastah : " << employee_map.searchByName("Pastah") << endl;
-    cout << "Phone Hp Ghana : " << employee_map.searchByName("Ghana") << endl;
+
     employee_map.remove("Mistah");
+
     cout << "Nomer Hp Mistah setelah dihapus : " << employee_map.searchByName("Mistah") << endl << endl;
+
     cout << "Hash Table : " << endl;
     employee_map.print();
+
     return 0;
 }
